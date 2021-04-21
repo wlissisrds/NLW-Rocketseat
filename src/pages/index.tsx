ace2423602f78216c40728b628c6ce0d6e40f474
wlissisrds <wlissisrds@outlook.com>
2021-04-21 18:33:19 -0300

    requisitando e lstando podcasts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,51 @@
-//SPA
-//SSR
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/converDurationToTimeString";
 
-import { useEffect } from "react"
+//style
+import style from "./home.module.scss";
 
-//SSG
-export default function Home(props) {
-  console.log(props.episodes)
+type Episode = {
+  id: string,
+  title: string,
+  thumbnail: string,
+  description: string,
+  members: string,
+  duration: number,
+  durationAsString: string,
+  url: string
+  publishedAt: string,
+}
+
+type HomeProps = {
+  //aray de objetos
+  latestEpisodes: Array<Episode>,
+  allEpisodes: Array<Episode>
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <>
-    index
-    </>
+    <div className={style.homepage}>
+      <section className={style.latestEpisodes}>
+        <h2>Ultimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <a href="">
+                  {episode.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      <section className={style.allEpisodes}>
+
+      </section>
+    </div>
+
   )
 }
 
@@ -23,31 +59,60 @@ export default function Home(props) {
   },[]);*/
 
 
-  //SSR
-  /*esse nume na função faz o next entender que precisa executar essa função
-  antes de exibir o conteudo da pagina pro usuário*/
-  /*export  async function getServerSideProps() {
-    const response = await fetch("http://localhost:3333/episodes");
-    const data = await response.json();
+/*//SSR
+esse nume na função faz o next entender que precisa executar essa função
+antes de exibir o conteudo da pagina pro usuário
+export  async function getServerSideProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
 
-    return {
-      props: { //props é nome obrigatorio alem do nome da funcao
-        episodes: data, //pode ser qualquer nome
-      }
-    }
-  }*/
-
-  /*SSG a unica coisa que muda é o nome da função e o objeto de retorno,
-   que agra possue o 
-   REVALIDATE (numero em segundos de quanto tempo eu quero gerar uma nova versao dessa pagina)*/
-  export  async function getStaticProps() {
-    const response = await fetch("http://localhost:3333/episodes");
-    const data = await response.json();
-
-    return {
-      props: { //props é nome obrigatorio alem do nome da funcao
-        episodes: data, //pode ser qualquer nome
-      },
-      revalidate: 60 * 60 * 8 //nesse caso em 8 em 8 horas
+  return {
+    props: { //props é nome obrigatorio alem do nome da funcao
+      episodes: data, //pode ser qualquer nome
     }
   }
+}*/
+
+/*SSG a unica coisa que muda é o nome da função e o objeto de retorno,
+ que agra possue o 
+ REVALIDATE (numero em segundos de quanto tempo eu quero gerar uma nova versao dessa pagina)*/
+export const getStaticProps: GetStaticProps = async () => {
+
+  //paramtros da request usando AXIOS
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  /*FORMATANDO OS DADOS DA API FORA DO COMPONENTE
+  PARA EVITAR PROBLEMAS NA HORA DO CARREGAMENTO*/
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: episode.published_at,
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    };
+  })
+
+  //cortanto o arrai de episodes
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length)
+
+
+  return {
+    props: { //props é nome obrigatorio alem do nome da funcao
+      latestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8 //nesse caso em 8 em 8 horas
+  }
+}
